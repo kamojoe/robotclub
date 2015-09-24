b6a0cd7dc176225491c61b3e155c27eb5b71d028
Cody Swendrowski <cody@swendrowski.us>
2015-09-24 13:10:01 -0500

    Revert "Revert "Initial push of Data I/O layer""
    This reverts commit 98656ff5799eecedca4c6cc6c07d65106e2d834f.

---
+++ Roomba/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roomba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var roomba = new RoombaController();
+
+            var result = roomba.Connect();
+
+            roomba.SwitchToFullMode();
+
+            roomba.Drive(300, 2001);
+
+            roomba.Disconnect();
+
+        }
+    }
+}
+
+
+
+
